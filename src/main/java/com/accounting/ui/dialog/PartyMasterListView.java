46e91c404d78c1794fc06bf9b153a2c3e50210ff
yashankt-philips <Yashank.Tiwari@philips.com>
2026-05-27 11:47:53 +0530

    fixing problems 22

--- src/main/java/com/accounting/ui/dialog/PartyMasterListView.java
+++ src/main/java/com/accounting/ui/dialog/PartyMasterListView.java
@@ -16,6 +16,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
